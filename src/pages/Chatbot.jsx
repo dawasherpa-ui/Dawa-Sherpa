add9f8c6c45049fe54c9d54d0f0c562479886eac
Dawa Sherpa <56500281+dawasherpa-ui@users.noreply.github.com>
2024-06-11 07:44:15 +0545

    Update Chatbot.jsx

--- src/pages/Chatbot.jsx
+++ src/pages/Chatbot.jsx
@@ -7,18 +7,18 @@ import { Howl } from "howler";
 import notificationSound from "../assets/discord.mp3"; // Import the sound file
 const fetchData = async (text, signal) => {
   const response = await fetch(
-    "https://chatbot-production-ae17.up.railway.app/chat",
+    "https://gemini-bot-k4ey.onrender.com/chat",
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ query: text }),
+      body: JSON.stringify({ question: text }),
       signal: signal, // Pass the signal to the fetch request
     }
   );
   const data = await response.json();
-  return data;
+  return data?.response;
 };
 
 function Chat() {
